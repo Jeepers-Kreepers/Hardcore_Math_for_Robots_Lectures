ff4cf5cc45d26ec8a732cc6b6273fa41f25bc0b5
Aleksandr <kraftsov@mail.com>
2025-02-12 22:39:34 +0300

    module_1 вычисление угла поворота

--- module_1/modeling_1.py
+++ module_1/modeling_1.py
@@ -14,7 +14,21 @@ ode = sympy.Eq(omega(t).diff(t),
 ode_sol = sympy.dsolve(ode, omega(t), ics={omega(0): 0})    # Решает уравнение исходя из начальных условий
 ode_num = ode_sol.subs({U: 8, T_m: 0.08, k_e: 0.5})         # Преобразует символьные выражения в числовые
 
+
+theta_eq = sympy.integrate(ode_sol.rhs, (t, 0, t))  # интегрирование уравнения для получения уравнения углового смещения
+theta = sympy.Function('theta')  # обозначение функции углового смещения
+
+ode_theta = sympy.Eq(theta(t).diff(t, 2), -theta(t).diff(t) / T_m + U / (T_m * k_e))  # дифференциальное уравнение углового смещения
+ode_sol_theta = sympy.dsolve(ode_theta, theta(t), ics={theta(0): 0, theta(t).diff(t).subs(t, 0): 0})  # решение дифференциального уравнения
+theta_num = ode_sol_theta.subs({k_e: 0.5, T_m: 0.08, U: 8})  # подстановка числовых значений в уравнение
+
+
+
+
 if __name__ == '__main__':
     t0 = float(input("Введите начальное время (ex = 0): "))
     t_end = float(input("Введите конечное время (ex = 0.5): "))
     plot(ode_num.rhs, (t, t0, t_end))     # Построение графика угловой скорости в зависимости от времени
+    plot(theta_num.rhs, (t, 0, 0.5), line_color='red',
+         title='Разгон двигателя', xlabel='time,[sec]',
+         ylabel='angle, [rad]')  # изображение графика с различными настройками
